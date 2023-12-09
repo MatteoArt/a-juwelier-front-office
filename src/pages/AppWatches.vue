d4942c60c8c0928abd1c05511971eeb9ddb4044c
devjuw <devjuwelier@mail.com>
2023-12-09 23:23:37 +0100

    axios

--- src/pages/AppWatches.vue
+++ src/pages/AppWatches.vue
@@ -1,7 +1,29 @@
 <script>
+import axios from 'axios';
+import WatchCard from '../components/WatchCard.vue';
 
 export default {
+    components: {
+        WatchCard,
+    },
+    data() {
+        return {
+            watches: []
+        }
+    },
+    methods: {
+        fetchData() {
+            axios.get('http://127.0.0.1:8000/api/watches')
+            .then((response) => {
+                let ris = response.data.results;
 
+                this.watches = ris;
+            })
+        }
+    },
+    mounted() {
+        this.fetchData();
+    }
 }
 
 </script>
@@ -9,6 +31,13 @@ export default {
 <template>
     <h1 class="text-center">Watches</h1>
 
+    <div class="container">
+        <div class="row">
+            <div v-for="watch in watches" class="col-md-3">
+                <WatchCard :watchData="watch"></WatchCard>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
