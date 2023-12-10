bf214740bc54260ef1639b57d3dbeb14c7cdbcf8
devjuw <devjuwelier@mail.com>
2023-12-10 22:19:42 +0100

    added show and not found

--- src/components/WatchCard.vue
+++ src/components/WatchCard.vue
@@ -7,25 +7,54 @@ export default {
             required: true
         }
     },
+    data() {
+        return {
+            arrImages: []
+        }
+    },
     methods: {
         getImagePath(img) {
             return new URL(`../assets/${img}`, import.meta.url).href;
+        },
+        convertToArr() {
+            var images = JSON.parse(this.watchData.images);
+            for (let i = 0; i < images.length; i++) {
+                this.arrImages.push(images[i]);
+            }
         }
+    },
+    mounted() {
+        this.convertToArr();
     }
 }
 
 </script>
 
 <template>
-    <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                content.</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="card h-100">
+        <div class="my-img-container">
+            <img :src="arrImages[0]" class="card-img-top" :alt="watchData.model">
+        </div>
+        <div class="card-body text-center d-flex flex-column">
+            <h5 class="card-title">{{ watchData.brand }}</h5>
+            <h6 class="card-subtitle mb-2 text-body-secondary">{{ watchData.model }}</h6>
+            <p class="card-text">
+                <div class="mb-1">Ref. {{ watchData.ref }}</div>
+                <span>€ {{ (watchData.price).toFixed(2).replace('.', ',') }}</span>
+            </p>
+            <div class="mt-auto">
+                <router-link :to="{ name: 'single-watch', params: { slug: watchData.slug } }" class="btn btn-outline-info">
+                    <i class="fa-solid fa-eye"></i>
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+
+.my-img-container {
+    height: 252px;
+    overflow: hidden;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -4,6 +4,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import AppHome from './pages/AppHome.vue'
 import AppWatches from './pages/AppWatches.vue'
 import AppContacts from './pages/AppContacts.vue'
+import SingleWatch from './pages/SingleWatch.vue'
+import NotFoundPage from './pages/NotFoundPage.vue'
 
 const routes = [
     {
@@ -17,9 +19,19 @@ const routes = [
         component: AppWatches
     },
     {
+        path: '/watch/:slug',
+        name: 'single-watch',
+        component: SingleWatch
+    },
+    {
         path: '/contacts',
         name: 'contacts',
         component: AppContacts
+    },
+    { //assicurati che il not found sia sempre alla fine dell'array routes perchè se nessuna rotta è stata catturata in quelle definite allora qualsiasi eccezione carica not found
+        path: '/:pathMatch(.*)*',
+        name: 'not-found',
+        component: NotFoundPage
     }
 ];
 
