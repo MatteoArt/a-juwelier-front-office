60f839da29427dd24c2528c19ac594b542157537
MatteoArt <mattrim8@gmail.com>
2023-12-22 22:12:50 +0100

    implement form sincronized v-model

--- src/pages/AppWatches.vue
+++ src/pages/AppWatches.vue
@@ -8,7 +8,8 @@ export default {
     },
     data() {
         return {
-            watches: []
+            watches: [],
+            searchForm: '',
         }
     },
     methods: {
@@ -32,6 +33,15 @@ export default {
     <h1 class="text-center">Watches</h1>
 
     <div class="container-fluid">
+        <form action="" class="w-50 my-5">
+            <div class="input-group">
+                <input type="text" class="form-control rounded-start-4" placeholder="Search for brand/model/ref"
+                v-model="searchForm">
+                <button type="submit" class="btn btn-outline-dark rounded-end-4 px-3">
+                    <i class="fa-solid fa-magnifying-glass"></i>
+                </button>
+            </div>
+        </form>
         <div class="d-flex flex-wrap">
             <div v-for="watch in watches" class="my-flex-item">
                 <WatchCard :watchData="watch"></WatchCard>
@@ -45,5 +55,9 @@ export default {
 .my-flex-item {
     flex-basis: calc((100% / 5) - (7px * 2));
     margin: 0 7px;
+}
+.form-control:focus {
+    border-color: black;
+    box-shadow:  0 0 0 0.15rem rgba(25, 25, 25, 0.63);
 }
 </style>
