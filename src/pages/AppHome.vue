3b69b33c345bc0fccb941cd8a676766549b4527e
MatteoArt <mattrim8@gmail.com>
2023-12-26 00:49:43 +0100

    fixed graphic

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -8,9 +8,16 @@ export default {
 
 <template>
     <h1 class="text-center">Home</h1>
-
+    <div class="my-img-logo-container">
+        <img src="../assets/a-juwelier.png" class="img-fluid" alt="a juwelier">
+    </div>
+    
 </template>
 
 <style scoped>
 
+.my-img-logo-container {
+    width: 400px;
+    margin: auto;
+}
 </style>
