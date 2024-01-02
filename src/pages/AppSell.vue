aadf2ab4e3434bc3d8133406b287b76a8b6ec57a
devjuw <devjuwelier@mail.com>
2024-01-03 00:22:23 +0100

    commit

--- src/pages/AppSell.vue
+++ src/pages/AppSell.vue
@@ -86,7 +86,7 @@ export default {
                 phoneError: '',
                 cityError: '',
                 addressError: '',
-                informationsError: ['','','','','','','','','','','',''],
+                informationsError: ['', '', '', '', '', '', '', '', '', '', '', ''],
                 photo1Error: '',
                 photo2Error: '',
                 photo3Error: '',
@@ -97,6 +97,7 @@ export default {
     },
     methods: {
         onFormSubmit() {
+            this.validatedData();
             console.log(this.formData)
             console.log(this.errorMessages)
         },
@@ -141,7 +142,75 @@ export default {
         },
         validatedData() {
             if (!this.formData.fullname) {
+                this.errorMessages.fullnameError = 'The full name field is required';
+            } else if (!isNaN(this.formData.fullname) || (/[0-9]/.test(this.formData.fullname))) { //se è un numero o contiene un numero
+                this.errorMessages.fullnameError = 'Please enter a valid name';
+            } else if (this.formData.fullname.length < 3) {
+                this.errorMessages.fullnameError = 'The name must contain at least 3 letters';
+            } else {
+                this.errorMessages.fullnameError = '';
+            }
 
+            if (!this.formData.email) {
+                this.errorMessages.emailError = 'The email field is required';
+            } else if (!isNaN(this.formData.email)) {
+                this.errorMessages.emailError = 'Please enter a valid email';
+            } else if (this.formData.email.length < 7) {
+                this.errorMessages.emailError = 'The email must contain at least 7 characters';
+            } else {
+                this.errorMessages.emailError = '';
+            }
+
+            if (!this.formData.phone) {
+                this.errorMessages.phoneError = 'The phone field is required';
+            } else if (!/^[\d ]*$/.test(this.formData.phone)) {
+                this.errorMessages.phoneError = 'Please enter a valid phone number';
+            } else if (this.formData.phone.length < 3) {
+                this.errorMessages.phoneError = 'The phone number must contain at least 3 digits';
+            } else {
+                this.errorMessages.phoneError = '';
+            }
+
+            if (this.formData.city && this.formData.city.length < 3) {
+                this.errorMessages.cityError = 'The city must contain at least 3 letters';
+            } else if (this.formData.city && (!isNaN(this.formData.city) || (/[0-9]/.test(this.formData.city)))) { //se è un numero o contiene un numero
+                this.errorMessages.cityError = 'Please enter a valid city';
+            } else {
+                this.errorMessages.cityError = '';
+            }
+
+            if (this.formData.address && this.formData.address.length < 5) {
+                this.errorMessages.addressError = 'The address must contain at least 5 characters';
+            } else if (this.formData.address && !isNaN(this.formData.address)) {
+                this.errorMessages.addressError = 'Please enter a valid address';
+            } else {
+                this.errorMessages.addressError = '';
+            }
+
+            for (let i = 0; i < this.errorMessages.informationsError.length; i++) {
+                if (this.formData.informations[i] && this.formData.informations[i].length < 2) {
+                    this.errorMessages.informationsError[i] = 'The field must contain at least 2 characters';
+                } else {
+                    this.errorMessages.informationsError[i] = '';
+                }
+            }
+
+            //qui validazione foto
+
+            if (!this.formData.price) {
+                this.errorMessages.priceError = 'The field is required';
+            } else if (!/[0-9]/.test(this.formData.price)) {
+                this.errorMessages.priceError = 'Please enter a valid price';
+            } else {
+                this.errorMessages.priceError = '';
+            }
+
+            if (this.formData.note && this.formData.note.length < 10) {
+                this.errorMessages.noteError = 'The message must contain at least 10 characters';
+            } else if (this.formData.note && !isNaN(this.formData.note)) {
+                this.errorMessages.noteError = 'Please enter a valid message';
+            } else {
+                this.errorMessages.noteError = '';
             }
         }
     }
@@ -157,23 +226,43 @@ export default {
             <legend class="col-12 m-0 fw-semibold">Personal informations</legend>
             <div class="col-md-6 mt-1">
                 <label for="inputName" class="form-label fw-semibold">Full Name</label>
-                <input type="text" class="form-control" id="inputName" v-model="formData.fullname">
+                <input type="text" class="form-control" id="inputName" v-model="formData.fullname"
+                    :class="errorMessages.fullnameError ? 'is-invalid' : ''">
+                <div v-if="errorMessages.fullnameError" class="invalid-feedback">
+                    {{ errorMessages.fullnameError }}
+                </div>
             </div>
             <div class="col-md-6 mt-1">
                 <label for="inputEmail" class="form-label fw-semibold">E-mail Address</label>
-                <input type="email" class="form-control" id="inputEmail" v-model="formData.email">
+                <input type="email" class="form-control" id="inputEmail" v-model="formData.email"
+                    :class="errorMessages.emailError ? 'is-invalid' : ''">
+                <div v-if="errorMessages.emailError" class="invalid-feedback">
+                    {{ errorMessages.emailError }}
+                </div>
             </div>
             <div class="col-md-4">
                 <label for="inputPhone" class="form-label fw-semibold">Phone Number</label>
-                <input type="tel" class="form-control" id="inputPhone" v-model="formData.phone">
+                <input type="tel" class="form-control" id="inputPhone" v-model="formData.phone"
+                    :class="errorMessages.phoneError ? 'is-invalid' : ''">
+                <div v-if="errorMessages.phoneError" class="invalid-feedback">
+                    {{ errorMessages.phoneError }}
+                </div>
             </div>
             <div class="col-md-4">
                 <label for="inputCity" class="form-label fw-semibold">City</label>
-                <input type="text" class="form-control" id="inputCity" v-model="formData.city">
+                <input type="text" class="form-control" id="inputCity" v-model="formData.city"
+                    :class="errorMessages.cityError ? 'is-invalid' : ''">
+                <div v-if="errorMessages.cityError" class="invalid-feedback">
+                    {{ errorMessages.cityError }}
+                </div>
             </div>
             <div class="col-md-4">
                 <label for="inputAddress" class="form-label fw-semibold">Address</label>
-                <input type="text" class="form-control" id="inputAddress" v-model="formData.address">
+                <input type="text" class="form-control" id="inputAddress" v-model="formData.address"
+                    :class="errorMessages.addressError ? 'is-invalid' : ''">
+                <div v-if="errorMessages.addressError" class="invalid-feedback">
+                    {{ errorMessages.addressError }}
+                </div>
             </div>
         </fieldset>
 
@@ -182,11 +271,15 @@ export default {
             <div v-for="(watch, i) in watchesFieldList" class="col-md-4">
                 <label :for="`inputInformationWatch${i}`" class="form-label fw-semibold">{{ watch.labelName }}</label>
                 <input v-if="!watch.options" type="text" class="form-control" :id="`inputInformationWatch${i}`"
-                    v-model="formData.informations[i]">
-                <select v-else class="form-select" :id="`inputInformationWatch${i}`" v-model="formData.informations[i]">
+                    v-model="formData.informations[i]" :class="errorMessages.informationsError[i] ? 'is-invalid' : ''">
+                <select v-else class="form-select" :id="`inputInformationWatch${i}`" v-model="formData.informations[i]"
+                    :class="errorMessages.informationsError[i] ? 'is-invalid' : ''">
                     <option value="" selected>Select</option>
                     <option v-for="option in watch.options" :value="option">{{ option }}</option>
                 </select>
+                <div v-if="errorMessages.informationsError[i]" class="invalid-feedback">
+                    {{ errorMessages.informationsError[i] }}
+                </div>
             </div>
             <div class="row g-3 mt-4">
                 <div class="col-md-4">
@@ -205,11 +298,19 @@ export default {
         </fieldset>
         <div class="col-md-6">
             <label for="inputPrice" class="form-label fw-semibold">Your price</label>
-            <input type="text" class="form-control" id="inputPrice" v-model="formData.price">
+            <input type="text" class="form-control" id="inputPrice" v-model="formData.price"
+                :class="errorMessages.priceError ? 'is-invalid' : ''">
+            <div v-if="errorMessages.priceError" class="invalid-feedback">
+                {{ errorMessages.priceError }}
+            </div>
         </div>
         <div class="col-md-6 mt-2">
             <label for="inputNote" class="form-label fw-semibold">Note</label>
-            <textarea class="form-control" id="inputNote" style="height: 150px;" v-model="formData.note"></textarea>
+            <textarea class="form-control" id="inputNote" style="height: 150px;" v-model="formData.note"
+            :class="errorMessages.noteError ? 'is-invalid' : ''"></textarea>
+            <div v-if="errorMessages.noteError" class="invalid-feedback">
+                {{ errorMessages.noteError }}
+            </div>
         </div>
         <button type="submit" class="btn btn-outline-success mt-3">Submit</button>
     </form>
